d6eddd93ff5f0c082f6de3c27c0e3e78f6fffae0
skdhinsa <serena.dhinsa@gmail.com>
2022-05-22 13:12:54 +0100

    change roundup to GET endpoint

--- src/main/java/com/example/serenakd/roundup/controller/roundUpController.java
+++ src/main/java/com/example/serenakd/roundup/controller/roundUpController.java
@@ -1,15 +1,11 @@
 package com.example.serenakd.roundup.controller;
 
-import com.example.serenakd.roundup.model.Account;
-import com.example.serenakd.roundup.service.Impl.AccountServiceImpl;
 import com.example.serenakd.roundup.service.Impl.RoundUpServiceImpl;
-import com.example.serenakd.roundup.service.Impl.TransactionsBetweenServiceImpl;
-import com.example.serenakd.roundup.service.TransactionsBetweenService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 public class roundUpController {
@@ -22,9 +18,9 @@ public class roundUpController {
      */
     @GetMapping("/roundup")
     @ResponseBody
-    public ResponseEntity<Integer> postRoundUp() throws Exception {
-        int response = roundUpService.putSweepAmountIntoSavingsGoal();
-        return new ResponseEntity<Integer>(response, HttpStatus.OK);
+    public ResponseEntity<?> postRoundUp() throws Exception {
+        roundUpService.putSweepAmountIntoSavingsGoal();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
