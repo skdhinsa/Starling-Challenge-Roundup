a22eb770b3c934bf9f2427ebd87a1ec0fc404677
skdhinsa <serena.dhinsa@gmail.com>
2022-05-23 17:02:38 +0100

    change function name

--- src/main/java/com/example/serenakd/roundup/service/SavingsGoalService.java
+++ src/main/java/com/example/serenakd/roundup/service/SavingsGoalService.java
@@ -6,7 +6,7 @@ import com.example.serenakd.roundup.model.SavingsGoals;
 import java.util.List;
 
 public interface SavingsGoalService{
-    String createNewSavingsGoal();
+    String createNewSavingsGoalUid();
     void addToSavingsGoal(String savingsGoalUid, int sweepingAmount);
     List<SavingsGoals> getAllSavingsGoals();
     boolean isGoalAlreadyPresent();
