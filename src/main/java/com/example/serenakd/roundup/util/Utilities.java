fa5132a58631b01b75d4200b2355341058d49692
skdhinsa <serena.dhinsa@gmail.com>
2022-05-23 14:54:27 +0100

    update for token

--- src/main/java/com/example/serenakd/roundup/util/Utilities.java
+++ src/main/java/com/example/serenakd/roundup/util/Utilities.java
@@ -6,13 +6,14 @@ import org.springframework.http.MediaType;
 
 import java.util.List;
 
+import static java.lang.String.format;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 public class Utilities {
 
     public static HttpEntity<String> createHttpHeaders(String bearerToken, HttpHeaders httpHeaders) {
         httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
-        httpHeaders.set(AUTHORIZATION, bearerToken);
+        httpHeaders.set(AUTHORIZATION, format("Bearer %s", bearerToken));
         return new HttpEntity<>(httpHeaders);
     }
 }
